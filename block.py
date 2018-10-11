fe0b66e8b04ed77c14fd1fcb30067bf68fb9678e
Erik Yi <lurk369@gmail.com>
2018-10-10 23:42:36 -0400

    adjust coding styling

--- block.py
+++ block.py
@@ -1,58 +1,58 @@
 class Block:
 
-	def __init__(self):
-		self.owner = None
-		self.owned_cells = [set(), set()]
+    def __init__(self):
+        self.owner = None
+        self.owned_cells = [set(), set()]
 
-	def put(self, i, p):
-		self.owned_cells[p].add(i)
-		self.check_win()
+    def put(self, i, p):
+        self.owned_cells[p].add(i)
+        self.check_win()
 
-	def check_win(self):
-		
-		for i in range(2):
-			if self._check_player(self.owned_cells[i]):
-				self.owner = i
-				break
+    def check_win(self):
 
-	def _check_player(self, owned):
-		# always check from smaller index to greater
-		for cell in [0,1,2,3,6]:
-			if cell in owned:
-				if self._check_win_cells(cell, owned):
-					return True
+        for i in range(2):
+            if self._check_player(self.owned_cells[i]):
+                self.owner = i
+                break
 
-		return False
+    def _check_player(self, owned):
+        # always check from smaller index to greater
+        for cell in [0,1,2,3,6]:
+            if cell in owned:
+                if self._check_win_cells(cell, owned):
+                    return True
 
-	def _check_win_cells(self, cell, owned):
-		"""
-		only check winning cells having greater indexes, because those with smaller indexes should be checked earlier
-		"""
-		if cell == 0:
-			return (1 in owned and 2 in owned) or (3 in owned and 6 in owned) or (4 in owned and 8 in owned)
-		if cell == 1:
-			return (4 in owned and 7 in owned)
-		if cell == 2:
-			return (4 in owned and 6 in owned) or (5 in owned and 8 in owned)
-		if cell == 3:
-			return (4 in owned and 5 in owned)
-		return (6 in owned and 7 in owned and 8 in owned)
+        return False
 
-	def to_string_array(self):
+    def _check_win_cells(self, cell, owned):
+        """
+        only check winning cells having greater indexes, because those with smaller indexes should be checked earlier
+        """
+        if cell == 0:
+            return (1 in owned and 2 in owned) or (3 in owned and 6 in owned) or (4 in owned and 8 in owned)
+        if cell == 1:
+            return 4 in owned and 7 in owned
+        if cell == 2:
+            return (4 in owned and 6 in owned) or (5 in owned and 8 in owned)
+        if cell == 3:
+            return 4 in owned and 5 in owned
+        return 6 in owned and 7 in owned and 8 in owned
 
-		cells = [[str(self.owner) for y in range(3)] for x in range(3)]
-		string_array = []  # contains three strings, each represents 1 row
+    def to_string_array(self):
 
-		if self.owner is None:
-			for i in range(3):
-				for j in range(3):
-					if 3*i+j in self.owned_cells[0]:
-						cells[i][j] = '0'
-					elif 3*i+j in self.owned_cells[1]:
-						cells[i][j] = '1'
-					else:
-						cells[i][j] = '.'
-		for row in cells:
-			string_array.append(' '.join(row))
+        cells = [[str(self.owner) for y in range(3)] for x in range(3)]
+        string_array = []  # contains three strings, each represents 1 row
 
-		return string_array
+        if self.owner is None:
+            for i in range(3):
+                for j in range(3):
+                    if 3*i+j in self.owned_cells[0]:
+                        cells[i][j] = '0'
+                    elif 3*i+j in self.owned_cells[1]:
+                        cells[i][j] = '1'
+                    else:
+                        cells[i][j] = '.'
+        for row in cells:
+            string_array.append(' '.join(row))
+
+        return string_array
--- board.py
+++ board.py
@@ -2,71 +2,67 @@ from block import Block
 
 
 class TenBoard:
+    P1 = 0
+    P2 = 1
+    PLAYERS = ['P1', 'P2']
 
-	P1 = 0
-	P2 = 1
-	PLAYERS = ['P1','P2']
+    def __init__(self):
+        self.blocks = [Block() for x in range(9)]
+        self.owned_blocks = [set(), set()]  # [p1_owned, p2_owned]
 
-	def __init__(self):
-		self.blocks = [Block() for x in range(9)]
-		self.owned_blocks = [set(), set()]  # [p1_owned, p2_owned]
+    def put(self, i, j, p):
+        self.blocks[i].put(j, p)
+        self.update_owner()
+        self.print_board()
+        return self.check_win()
 
-	def put(self, i, j, p):
-		self.blocks[i].put(j, p)
-		self.update_owner()
-		self.print_board()
-		return self.check_win()
+    def update_owner(self):
+        for i in range(9):
+            if self.blocks[i].owner is not None:
+                self.owned_blocks[self.blocks[i].owner].add(i)
 
-	def update_owner(self):
-		for i in range(9):
-			if self.blocks[i].owner is not None:
-				self.owned_blocks[self.blocks[i].owner].add(i)
+    def check_win(self):
 
-	def check_win(self):
-		
-		for i in range(2):
-			if self._check_player(self.owned_blocks[i]):
-				return self.PLAYERS[i]
-		return None
+        for i in range(2):
+            if self._check_player(self.owned_blocks[i]):
+                return self.PLAYERS[i]
+        return None
 
-	def _check_player(self, owned):
-		# always check from smaller index to greater
-		for block in [0,1,2,3,6]:
-			if block in owned:
-				if self._check_win_blocks(block, owned):
-					return True
+    def _check_player(self, owned):
+        # always check from smaller index to greater
+        for block in [0, 1, 2, 3, 6]:
+            if block in owned:
+                if self._check_win_blocks(block, owned):
+                    return True
 
-		return False
+        return False
 
-	def _check_win_blocks(self, block, owned):
-		"""
-		only check winning blocks having greater indexes, because those with smaller indexes should be checked earlier
-		"""
-		if block == 0:
-			return (1 in owned and 2 in owned) or (3 in owned and 6 in owned) or (4 in owned and 8 in owned)
-		if block == 1:
-			return (4 in owned and 7 in owned)
-		if block == 2:
-			return (4 in owned and 6 in owned) or (5 in owned and 8 in owned)
-		if block == 3:
-			return (4 in owned and 5 in owned)
-		return (6 in owned and 7 in owned and 8 in owned)
+    def _check_win_blocks(self, block, owned):
+        """
+        only check winning blocks having greater indexes, because those with smaller indexes should be checked earlier
+        """
+        if block == 0:
+            return (1 in owned and 2 in owned) or (3 in owned and 6 in owned) or (4 in owned and 8 in owned)
+        if block == 1:
+            return 4 in owned and 7 in owned
+        if block == 2:
+            return (4 in owned and 6 in owned) or (5 in owned and 8 in owned)
+        if block == 3:
+            return 4 in owned and 5 in owned
+        return 6 in owned and 7 in owned and 8 in owned
 
-	def print_board(self):
+    def print_board(self):
 
-		board_string_array = []  # contains three strings, each represents 3 rows
-		for i in range(3):
-			block_string_array = []  # contains three strings, each represents 1 row
-			block_cells = []
-			for j in range(3):
-				block_cells.append(self.blocks[3*i+j].to_string_array())
+        board_string_array = []  # contains three strings, each represents 3 rows
+        for i in range(3):
+            block_string_array = []  # contains three strings, each represents 1 row
+            block_cells = []
+            for j in range(3):
+                block_cells.append(self.blocks[3 * i + j].to_string_array())
 
-			for k in range(3):
-				block_string_array.append('|'.join([block_cells[0][k], block_cells[1][k], block_cells[2][k]])+'\n')
+            for k in range(3):
+                block_string_array.append('|'.join([block_cells[0][k], block_cells[1][k], block_cells[2][k]]) + '\n')
 
-			board_string_array.append(''.join(block_string_array))
+            board_string_array.append(''.join(block_string_array))
 
-		print '-----+-----+-----\n'.join(board_string_array)
-
-
-
+        print '-----+-----+-----\n'.join(board_string_array)
